ae745a476bcf21cf9f97deb43f60a03424c06390
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-16 09:30:00 +0100

    +client build

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const request = require('request');
 const apiUrl = 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json'
 
 app.use(express.json())
-app.use(express.static('client/public')) // prod
+app.use(express.static('client/build')) // prod
 
 // res is a json url
 app.get('/api', (req, res) => {
@@ -23,7 +23,7 @@ app.get('/api', (req, res) => {
 
 // prod: resolve any address with built index.html (where spa root lives)
 app.get('/*', (_, res) => {
-    res.sendFile(path.join(__dirname, './client/public/index.html'))
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 
